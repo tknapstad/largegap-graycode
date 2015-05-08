fc37f5e34f3df80d5818fcba03de19d192389356
Tarjei Knapstad <tarjeik@norbit.no>
2015-05-08 17:10:16 +0200

    Added ability to create C lookup table

--- src/examples.cpp
+++ src/examples.cpp
@@ -12,10 +12,17 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 #include <math.h>
 #include "LargeGapGrayCode.h"
 
-void printBinaryCode(const std::vector<std::bitset<20> >& code, int bitWidth);
+void printBinaryCode(const std::vector<std::bitset<20> >& code, int bitWidth, std::ostream &stream);
+void printBinaryCodeVertical(const std::vector<std::bitset<20> >& code, int bitWidth, std::ostream &stream);
+void printBinaryCodeCArray(const std::vector<std::bitset<20> >& code, int bitWidth, std::ostream &stream);
+
+void printBinaryCodeToFile(const std::vector<std::bitset<20> >& code, int bitWidth, const char* filename, bool vertical = false);
+void printBinaryCodeToCFile(const std::vector<std::bitset<20> >& code, int bitWidth, const char* filename);
 
 int main() {
 	LargeGapGrayCode lggc;
@@ -37,27 +44,88 @@ int main() {
 	lggc.printStatistics(16);
 	lggc.createCodeFromTheorem1(11, 5, 21, 11);
 	lggc.printStatistics(16);
+	lggc.printStatistics(13);
 
 	std::cout << std::endl << std::endl;
 	std::cout << "####################################################################" << std::endl;
 	std::cout << "#######       3)  Printing generated 7-bit binary code       #######" << std::endl;
 	std::cout << "#######             Note: MinGap = 5 and MaxGap = 11         #######" << std::endl;
 	std::cout << "####################################################################" << std::endl << std::endl;
-	printBinaryCode(lggc.getBinaryCode(7), 7);
+	printBinaryCode(lggc.getBinaryCode(7), 7, std::cout);
+    //	printBinaryCodeVertical(lggc.getBinaryCode(7), 7, std::cout);
+
+	printBinaryCodeToFile(lggc.getBinaryCode(16), 16, "16bitcode.txt", true);
+	printBinaryCodeToFile(lggc.getBinaryCode(13), 13, "13bitcode.txt", true);
+	printBinaryCodeToCFile(lggc.getBinaryCode(13), 13, "large_gap_gray_code_13bit.c");
 
 	return 0;
 }
 
 
-void printBinaryCode(const std::vector<std::bitset<20> >& code, int bitWidth)
+void printBinaryCode(const std::vector<std::bitset<20> >& code, int bitWidth, std::ostream &stream)
 {
 	for(int j = 0; j < bitWidth; ++j)
 	{
 		for(int i = 0; i < pow(2, bitWidth); i++)
 		{
-		    std::cout << code[i][j];
+		    stream << code[i][j];
 		}
-		std::cout << std::endl;
+		stream << std::endl;
 	}
-	std::cout << std::endl;
+	stream << std::endl;
+}
+
+void printBinaryCodeVertical(const std::vector<std::bitset<20> >& code, int bitWidth, std::ostream &stream)
+{
+    for(int i = 0; i < pow(2, bitWidth); i++)
+	{
+        for(int j = 0; j < bitWidth; ++j)
+        {
+		    stream << code[i][j];
+		}
+		stream << std::endl;
+	}
+	stream << std::endl;
+}
+
+void printBinaryCodeCArray(const std::vector<std::bitset<20> >& code, int bitWidth, std::ostream &stream)
+{
+    int maxNum = pow(2, bitWidth);
+    stream << "unsigned int lggc_" << bitWidth << "[" << maxNum << "] = {" << std::endl;
+    int fieldWidth = static_cast<int>(ceil(bitWidth/4.L));
+    for(int i = 0; i < maxNum; i++)
+	{
+        stream << "\t0x" << std::setfill('0') << std::setw(fieldWidth) << std::hex << code[i].to_ulong();
+        if (i < maxNum - 1) {
+            stream << ",";
+        }
+        stream << std::endl;
+	}
+    stream << "};" << std::endl;
+}
+
+void printBinaryCodeToFile(const std::vector<std::bitset<20> >& code, int bitWidth, const char* filename, bool vertical)
+{
+    std::ofstream myfile;
+    myfile.open (filename);
+    if (myfile.is_open() ) {
+        if (vertical) {
+            printBinaryCodeVertical(code, bitWidth, myfile);
+        } else {
+            printBinaryCode(code, bitWidth, myfile);
+        }
+    }
+    myfile.close();
+}
+
+void printBinaryCodeToCFile(const std::vector<std::bitset<20> >& code, int bitWidth, const char* filename)
+{
+    std::ofstream myfile;
+    myfile.open (filename);
+    if (myfile.is_open() ) {
+        printBinaryCodeCArray(code, bitWidth, myfile);
+    } else {
+        std::cerr << "FAiled to write file: "<< filename << std::endl;
+    }
+    myfile.close();
 }
